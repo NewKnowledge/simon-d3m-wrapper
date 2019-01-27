f4b05cd4b576f78aa7216fe1b4950aad3cf26903
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-01-27 14:59:33 -0600

    update python path

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='SimonD3MWrapper',
     ],
     entry_points = {
         'd3m.primitives': [
-            'distil.simon = SimonD3MWrapper:simon'
+            'data_cleaning.annotate_semantic_types.Simon = SimonD3MWrapper:simon'
         ],
     },
 )
