ff5b4dd18e3c7abd84ff6cdfc8be95151d8de617
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-01-28 11:20:03 -0600

    bump Simon dependency so it's in line with new D3M release

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(name='SimonD3MWrapper',
     install_requires=["numpy==1.15.4",
         "pandas==0.23.4",
         "typing==3.6.6",
-        "Simon==1.2.1"],
+        "Simon==1.2.3"],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/simon@d559abda8137fd6e351137a2a66d0464f67b718e#egg=Simon-1.2.1"
+        "git+https://github.com/NewKnowledge/simon@jg/editDeps#egg=Simon-1.2.3"
     ],
     entry_points = {
         'd3m.primitives': [
