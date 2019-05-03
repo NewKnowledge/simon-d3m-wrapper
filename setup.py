57993b08183a3282680f9c34325a92814e0099c4
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-05-03 11:06:32 -0500

    latest d3m-simon commit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='SimonD3MWrapper',
     install_requires=["numpy==1.15.4",
         "pandas==0.23.4",
         "typing==3.6.6",
-        "Simon @ git+https://github.com/NewKnowledge/simon@722ccf972fc76f48386135a97ea75f2dd1a9504b#egg=Simon-1.2.4"],
+        "Simon @ git+https://github.com/NewKnowledge/simon@4016724bcc37c94622aa8b36234ebaa0d9a57ed8#egg=Simon-1.2.4"],
     entry_points = {
         'd3m.primitives': [
             'data_cleaning.column_type_profiler.Simon = SimonD3MWrapper:simon'
