50a15017013d1cde37ab2c44ec5a84486accd239
jgleason <jeffrey.gleason@newknowledge.io>
2019-11-22 16:26:34 -0600

    latest commit from simon base

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='SimonD3MWrapper',
-    version='1.2.1',
+    version='1.2.2',
     description='A thin client for interacting with simon data type classifier',
     packages=['SimonD3MWrapper'],
     install_requires=["numpy>=1.15.4,<=1.17.3",
         "pandas>=0.23.4,<=0.25.2",
         "typing==3.6.6",
-        "Simon @ git+https://github.com/NewKnowledge/simon@88a8031a81d568bc575373b789eea1a9909ff713#egg=Simon-1.2.4"],
+        "Simon @ git+https://github.com/NewKnowledge/simon@fe41f4eb4f3af848841b325323fc7fc01cd7711b#egg=Simon-1.2.4"],
     entry_points = {
         'd3m.primitives': [
             'data_cleaning.column_type_profiler.Simon = SimonD3MWrapper:simon'
