93a56e5f18485d4149cf57eeaa1c665259e7ea7d
jgleason <jeffrey.gleason@newknowledge.io>
2019-11-22 15:09:18 -0600

    fix setup file brackets

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='SimonD3MWrapper',
     packages=['SimonD3MWrapper'],
     install_requires=["numpy>=1.15.4,<=1.17.3",
         "pandas>=0.23.4,<=0.25.2",
-        "typing==3.6.6"],
+        "typing==3.6.6",
         "Simon @ git+https://github.com/NewKnowledge/simon@88a8031a81d568bc575373b789eea1a9909ff713#egg=Simon-1.2.4"],
     entry_points = {
         'd3m.primitives': [
