9e3197c9f47d781fec18fbee710cc06334ddc792
jgleason <jeffrey.gleason@newknowledge.io>
2020-01-06 15:05:45 -0600

    remove hardcoded typing dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='SimonD3MWrapper',
     packages=['SimonD3MWrapper'],
     install_requires=["numpy>=1.15.4,<=1.17.3",
         "pandas>=0.23.4,<=0.25.2",
-        "typing==3.6.6",
+        "typing",
         "Simon @ git+https://github.com/NewKnowledge/simon@fe41f4eb4f3af848841b325323fc7fc01cd7711b#egg=Simon-1.2.4"],
     entry_points = {
         'd3m.primitives': [
